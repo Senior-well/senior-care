4ecbb4b5b0de3f02c9a166b6e288cced6c039c0e
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2025-01-03 10:10:36 -0500

    Updated DropBox Components

--- src/frontend/Components/DropDownNav/DropDown.jsx
+++ src/frontend/Components/DropDownNav/DropDown.jsx
@@ -1,10 +1,16 @@
-import MenuItems from "../MenuItems/MenuItems";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Dropdown = ({ submenus, dropdown }) => {
     return (
         <ul className={`dropdown ${dropdown ? "show" : ""}`}>
             {submenus.map((submenu, index) => (
-                <MenuItems items={submenu} key={index} />
+                <li key={index}>
+                    <span className="menu-icon"><FontAwesomeIcon icon={submenu.icon} /></span>
+                    <div className="menu-text">
+                        <span className="menu-title">{submenu.title}</span>
+                        <span className="menu-description">{submenu.description}</span>
+                    </div>
+                </li>
             ))}
         </ul>
     );
