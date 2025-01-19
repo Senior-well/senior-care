352012dd77edf2749f6d6d2c390fd20edd848843
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2025-01-18 22:55:19 -0500

    Updated Image component

--- src/images/Images.jsx
+++ src/images/Images.jsx
@@ -7,9 +7,10 @@ import sw4 from './homePage/sw4.avif';
 import sw5 from './homePage/sw5.avif';
 import sw6 from './homePage/sw6.webp';
 import sw7 from './homePage/sw7.jpg';
+import map from './map/world.svg'
 import peter from './testData/Peter.jpg';
 import muhsin from './testData/Muhsin.jpg';
 import peyton from './testData/Peyton.jpg';
 import muntasir from './testData/Muntasir.jpg';
 
-export { logo, logoTrans, peter, muhsin, peyton, muntasir, sw1, sw2, sw3, sw4, sw5, sw6, sw7 };
+export { logo, logoTrans, peter, muhsin, peyton, muntasir, sw1, sw2, sw3, sw4, sw5, sw6, sw7, map };
