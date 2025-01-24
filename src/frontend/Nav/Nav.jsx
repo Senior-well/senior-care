2d308ce5cf7a12141f046b1382405f2b1ca33e46
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2025-01-24 13:53:33 -0500

    Updated employee login

--- src/frontend/Nav/Nav.jsx
+++ src/frontend/Nav/Nav.jsx
@@ -4,11 +4,14 @@ import { menuData } from "../../backend/MenuData/menuData";
 import MenuItems from "../Components/MenuItems/MenuItems";
 import Dropdown from "../Components/DropDownNav/DropDown";
 import { logoTrans } from "../../images/Images";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Nav() {
     const [dropdown, setDropdown] = useState({ visible: false, submenus: [] });
     const [translateNav, setTranslateNav] = useState(false);
     const lastScrollY = useRef(0);
+    const [loginDropDown, setLoginDropDown] = useState('');
 
     const handleMouseEnter = (submenus) => {
         if (submenus) {
@@ -20,11 +23,21 @@ export default function Nav() {
         setDropdown({ visible: false, submenus: [] });
     };
 
+    const handleButton = () => {
+        if (loginDropDown === 'show') {
+            setLoginDropDown('');
+        }
+        else {
+            setLoginDropDown('show');
+        }
+    }
+
     useEffect(() => {
         const handleScroll = () => {
             const currentScrollY = window.scrollY;
             if (currentScrollY > lastScrollY.current) {
                 setTranslateNav(true);
+                setLoginDropDown('');
             }
             else {
                 setTranslateNav(false);
@@ -62,8 +75,24 @@ export default function Nav() {
                         />)
                     }
                 </ul>
-                <div>
-                    <a className="waitlist" href="#">Join waitlist</a>
+
+                <div className="employee-login">
+                    <div className="login-container">
+                        <a className="login" href="#" onClick={(e) => (e.preventDefault(), handleButton())}><FontAwesomeIcon icon={faCircleUser} /> Employee login</a>
+                    </div>
+                    <div className={`login-dropdown ${loginDropDown}`}>
+                        <form action="#">
+                            <div>
+                                <label htmlFor="employeeID">Employee ID</label>
+                                <input type="text" id="employeeID" placeholder="#####" />
+                            </div>
+                            <div>
+                                <label htmlFor="password">Password</label>
+                                <input type="password" placeholder="Password" />
+                            </div>
+                            <button type="submit" onClick={(e) => e.preventDefault()}>Login</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </nav>
