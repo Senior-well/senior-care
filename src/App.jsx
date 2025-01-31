a08d1f1ef5499181dc0197b8fea85d6a2b56de73
Peyton Da Silva <dasi0110@algonquinlive.com>
2025-01-30 20:55:23 -0500

    Updates to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { React } from 'react';
+import React, { useState } from 'react';
 import './App.sass';
-import Nav from './frontend/Nav/Nav';
-import HomePage from './frontend/HomePage/HomePage';
-import Footer from './frontend/Footer/Footer';
+import Nav from './frontend/Nav/Nav'; // Homepage navbar
+import HomePage from './frontend/HomePage/HomePage'; // Homepage content
+import Caregiver from './frontend/Caregiver/Caregiver'; // Caregiver page content
+import Footer from './frontend/Footer/Footer'; // Shared footer
+import Login from './frontend/Login/Login'; // Shared footer
+import Reminder from './frontend/Reminder/Reminder'; // Shared footer
+
+
 
 export default function App() {
   return (
     <div className='App'>
       <div className='Main-content'>
-        <Nav />
-        <HomePage />
+        {/* <Nav /> */}
+        {/* <Caregiver /> */}
+        {/* <Login /> */}
+        <Reminder />
       </div>
       <Footer />
     </div >
